6186c5e881c6e81e072ed248279c97144d74d24d
munemsarwar2016 <munemsarwar2016@gmail.com>
2025-12-16 17:36:25 +0600

    Initial commit

---
+++ class_1/practice_1.py
@@ -0,0 +1,12 @@
+#concatination using for loop
+
+num_of_input = int(input("How many strings we want to concatenate? ")) #user input from terminal but just for how many strings
+ 
+result=""  #initializing empty string
+
+for i in range(num_of_input):
+    user_input = input(f"Enter String {i+1}: ") #user input from terminal
+    result += user_input #concatenation of the strings
+
+
+print("The result is : ", result)
---
+++ class_1/sum_of_numbers.py
@@ -0,0 +1,11 @@
+#we want to sum 1 to 10
+
+sum = 0
+
+for i in range(1,11): # 1 intitialize point 1 to less then 11 so basically till 1 to 10
+    sum += i #sum here
+
+print("sum value from 1 to 10 is: ",sum) #print
+
+
+#1+2+3+4+5+6+7+8+9+10 = 55
